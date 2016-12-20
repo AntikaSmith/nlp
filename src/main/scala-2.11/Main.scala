269575c2ba912d465d52a8a7e67d7acb7abc346d
zhanghan <han_zh@sina.cn>
2016-12-20 20:50:54 +0800

    split the dataset into training set and test set

---
+++ src/main/scala-2.11/Main.scala
@@ -0,0 +1,10 @@
+/**
+  * Created by Anckaro on 2016/12/20.
+  */
+object Main {
+  def main(args: Array[String]) = {
+    Preprocess.splitDataset
+  }
+
+
+}
---
+++ src/main/scala-2.11/Preprocess.scala
@@ -0,0 +1,38 @@
+import java.io.PrintWriter
+
+/**
+  * Created by Anckaro on 2016/12/20.
+  * preprocess the orginal data, including split training/test set and adjudge the data format
+  */
+object Preprocess {
+  def readFileAsStringList(file: String) = {
+    val originalStream = getClass.getClassLoader.getResourceAsStream(file)
+    val rs = scala.io.Source.fromInputStream(originalStream).getLines().toList
+    originalStream.close()
+    rs
+  }
+
+  def writeFile(name: String, list: List[String]) = {
+    val file = new PrintWriter(name)
+    list.foreach(s => file.println(s))
+    file.close()
+  }
+
+  def splitDataset = {
+    val orginal = Preprocess.readFileAsStringList("BioCreative V.5 training set.txt")
+    val annotation = Preprocess.readFileAsStringList("CEMP_BioCreative V.5 training set annot.tsv")
+    val docIds = annotation.map(_.split("\t").apply(0)).toSet
+    val (trainIds, testIds) = util.Random.shuffle(docIds).splitAt((docIds.size * 0.8).toInt)
+    println(trainIds.size + "\n" + testIds.size)
+    def splitAndWrite(resultName: String, srcData: List[String], train: Set[String], test: Set[String]) = {
+      val idZipData = srcData.map(_.split("\t").apply(0)).zip(srcData)
+      val trainData = idZipData.filter(tuple => train.contains(tuple._1)).map(_._2)
+      val testData = idZipData.filter(tuple => test.contains(tuple._1)).map(_._2)
+      writeFile("target/" + resultName + "_train.tsv", trainData)
+      writeFile("target/" + resultName + "_test.tsv", testData)
+    }
+
+    splitAndWrite("original", orginal, trainIds, testIds)
+    splitAndWrite("annotation", annotation, trainIds, testIds)
+  }
+}
